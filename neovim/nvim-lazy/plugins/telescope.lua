a1114c4aaaa207fbb873a4aab50e19408e354dce
evo012 <71935756+evo012@users.noreply.github.com>
2026-07-01 22:40:06 +0200

    01-07-2026

--- neovim/nvim-lazy/plugins/telescope.lua
+++ neovim/nvim-lazy/plugins/telescope.lua
@@ -23,6 +23,24 @@ return {
     require("telescope").setup(opts)
     require("telescope").load_extension("fzf")
   end,
+  require("telescope").setup({
+    defaults = {
+      -- Estrategia visual: "horizontal" (por defecto), "vertical", "center" o "flex"
+      layout_strategy = "horizontal",
+      layout_config = {
+        -- Dimensiones globales (0.80 significa 80% de la pantalla)
+        width = 0.90, -- Ancho total de la ventana de Telescope
+        height = 0.85, -- Alto total de la ventana de Telescope
+        -- Control de la vista previa (Preview)
+        preview_width = 0.50, -- El 50% del ancho total será para la vista previa
+        preview_cutoff = 120, -- Si la pantalla mide menos de 120 columnas, oculta la vista previa
+        -- Posición de la barra de búsqueda y elementos internos
+        prompt_position = "bottom", -- Coloca el buscador arriba ("top") o abajo ("bottom")
+      },
+      -- Configuración para que el prompt arriba funcione correctamente
+      sorting_strategy = "ascending",
+    },
+  }),
   keys = {
     {
       "<leader>tf",
